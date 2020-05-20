92b5108725dad414b3275d6fc4be284eaf433e2e
Adam Heins <mail@adamheins.com>
2020-05-20 12:23:11 -0400

    Add script to create required symlinks for vim.

---
+++ symlink.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+vim_dir=${0:a:h}
+
+# Create a symlink at location $1 with source $2. Backup $1 if it already
+# exists.
+backup-and-link() {
+  [ -z "$2" ] && return 1
+
+  if [ -e "$1" ]; then
+    echo "Moved $1 -> $1.back"
+    mv "$1" "$1".back
+  fi
+
+  ln -s "$2" "$1"
+  echo "Symlinked $1 -> $2"
+}
+
+backup-and-link ~/.vim   "$vim_dir"
+backup-and-link ~/.vimrc "$vim_dir"/vimrc
